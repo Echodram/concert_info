68239df87085975a9e646a03615d1f323bc3aa75
Echodram <mardoche.kratos@gmail.com>
2025-06-16 22:14:25 +0100

    ...

--- concert/models.py
+++ concert/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Information(models.Model):
-    nom = models.CharField(max_length=250)
-    telephone = models.CharField(max_length=25)
+    nom = models.CharField(max_length=250, unique=True)
+    telephone = models.CharField(max_length=25, unique=True)
     recevoirInfo = models.BooleanField(default=False)
 
     
