844ad6de206c4104a036d917c30b06bdd71db017
Echodram <mardoche.kratos@gmail.com>
2025-06-19 16:45:26 +0100

    ...

--- concert_info/settings.py
+++ concert_info/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^%r5*i1be_62hr5-lfs&e8qvttmyrri3^nfd^g$u@m^cj-3&=)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["moldedinchrist.pythonanywhere.com/"]
+ALLOWED_HOSTS = ["moldedinchrist.pythonanywhere.com"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
