560537bada63174a291227d0bd4b1cce4749d886
Echodram <mardoche.kratos@gmail.com>
2025-06-19 18:13:45 +0100

    ...

--- concert/views.py
+++ concert/views.py
@@ -13,7 +13,6 @@ from django.conf import settings
 from PIL import Image, ImageDraw, ImageFont
 
 
- 
 
 def add_rotated_text_to_image(image_path, output_path, name, ticket_number):
     # Open the original image
@@ -48,9 +47,22 @@ def add_rotated_text_to_image(image_path, output_path, name, ticket_number):
 
     # Combine the original image with the text
     combined = Image.alpha_composite(base, txt)
+    width, height = combined.size
+    scale = 0.5  # Start with 50% size reduction
+    img_resized = combined.resize((int(width * scale), int(height * scale)), Image.LANCZOS)
+    img_palette = img_resized.convert("P", palette=Image.ADAPTIVE, colors=256)
+    
+    img_palette.save(output_path, format='PNG', optimize=True)
 
-    # Save the image
-    combined.save(output_path, "PNG")
+   
+def compress_image(input_path, output_path, quality=85):
+    with Image.open(input_path) as img:
+        if img.format == 'JPEG':
+            img.save(output_path, 'JPEG', quality=quality, optimize=True)
+        elif img.format == 'PNG':
+            img.save(output_path, 'PNG', optimize=True)
+        else:
+            img.save(output_path)
 
 
 class PostInforamtionView(APIView):
@@ -71,16 +83,13 @@ class PostInforamtionView(APIView):
         
         if serializer.is_valid():
             info = serializer.save()
-            download_path = os.path.join(settings.MEDIA_ROOT, f'invitation_{info.id}.png')
+            download_path = os.path.join(settings.MEDIA_ROOT, f'invitation_sent/invitation_{info.id}.png')
             if info.id <10:
                 id = '0' + str(info.id)
                 add_rotated_text_to_image(file_path, download_path, nom, id)
             else:
                 add_rotated_text_to_image(file_path, download_path, nom, info.id)
                 pass
-            download_path = os.path.join(settings.MEDIA_ROOT, f'invitation_{info.id}.png')
-            
-            image_url = f"{request.scheme}://{request.get_host()}{settings.MEDIA_URL}{download_path}"
             return Response({"id" : info.id ,
                              "nom" : info.nom,
                              'download_link': f'{host_uri}/invitation/{info.id}'
@@ -123,8 +132,9 @@ def get_file(request):
         else:
             raise Http404("File does not exist")
 
+
 def download_file(request, id):
-    file_path = os.path.join(settings.MEDIA_ROOT, f'invitation_{id}.png')
+    file_path = os.path.join(settings.MEDIA_ROOT, f'invitation_sent/invitation_{id}.png')
     if os.path.exists(file_path):
         response = FileResponse(open(file_path, 'rb'))
         response['Content-Disposition'] = f'attachment; filename= invitation_{id}.png'
